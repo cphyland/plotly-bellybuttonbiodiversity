78fd2055901efad4d2f0ed6dbcfba501917b1962
cphyland <court.hyland@gmail.com>
2021-07-07 21:15:56 -0500

    push

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -35,3 +35,10 @@ function plotFunctions() {
   bubbleChart(valueSelect);
   gaugeChart(valueSelect);
 }
+
+function demographicFunc(valueSelect) {
+    var filterValue2 = data.samples.filter(value => value.id == valueSelect);
+    var ouid = filterValue2.map(v => v.otu_ids);
+    ouid = treatOuid(ouid[0].slice(0, 10));
+    var valueX = filterValue2.map(v => v.sample_values);
+    valueX = valueX[0].slice(0, 10);
