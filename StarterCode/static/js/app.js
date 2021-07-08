b2b6e83a03d938bb6dff9c61a85aa98f604511a1
cphyland <court.hyland@gmail.com>
2021-07-07 21:03:26 -0500

    push

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,2 +1,9 @@
 //define variable
 var data;
+
+//function to fill in the select option
+
+function init() {
+    d3.json("JS/data/samples.json").then(dataInitial => {
+      data = dataInitial;
+      var selectValues = dataInitial.names;
